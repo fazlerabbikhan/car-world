ad75a5c5bcc43987dd8b961be8cc2c6309f48591
Fazle Rabbi <you@example.com>
2022-03-27 23:46:39 +0600

    finished

--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -61,6 +61,22 @@ const Cars = () => {
                     choose1ForMe={choose1ForMe}
                 ></Cart>
             </div>
+            <section className='quesAns'>
+                <h3>Q1: How React works?</h3>
+                <h4>Ans: Instead of using the traditional DOM class, React uses className.
+                    JSX tags have a name, children, and attributes. Numeric values and expressions must be written inside curly brackets. The quotation marks in JSX attributes represent strings, similarly to JavaScript.
+                    In most cases, React is written using JSX instead of standard JavaScript to simplify components and keep code clean.</h4>
+
+                <h3>Q2: What are the differences between Props and States?</h3>
+                <h4>Ans: In Props, the data is passed from one component to another but in State the data is passed within the component only.
+                    Props are immutable, States are mutable.
+                    Props can be used with state and functional components, State can be used only with the state components/class component.
+                    Props are read-only, State is both read and write.
+                </h4>
+
+                <h3>Q3: How useState works?</h3>
+                <h4>Ans: useState is a Hook that allows you to have state variables in functional components. You pass the initial state to this function and it returns a variable with the current state value (not necessarily the initial state) and another function to update this value.</h4>
+            </section>
         </div>
     );
 };
